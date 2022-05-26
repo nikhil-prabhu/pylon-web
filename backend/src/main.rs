ade947528b5f57e1bb8173423e20be589d30eced
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2022-05-26 12:15:53 +0000

    Set log level to normal for release mode

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,6 +7,12 @@ use rocket::{launch, routes};
 use rocket::fs::FileServer;
 
 #[cfg(not(debug_assertions))]
+use rocket::log::LogLevel;
+
+#[cfg(not(debug_assertions))]
+use rocket::Config;
+
+#[cfg(not(debug_assertions))]
 use std::env;
 
 // When run in production (release) mode, we serve the frontend's static files.
@@ -16,6 +22,10 @@ fn rocket() -> _ {
     let static_dir =
         env::var("PYLON_STATIC_DIR").expect("environment variable PYLON_STATIC_DIR not set");
     rocket::build()
+        .configure(Config {
+            log_level: LogLevel::Normal,
+            ..Config::release_default()
+        })
         .attach(fairings::CORSFairing)
         .mount("/", routes![routes::code, routes::send, routes::receive])
         .mount("/", FileServer::from(static_dir))
